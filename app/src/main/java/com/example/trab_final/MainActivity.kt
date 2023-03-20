ed7fed4b4a391926aa01cafe0da8b8e0ecb3f7b6
Robson Freitas <robson.flavio20@outlook.com>
2023-03-20 20:47:43 -0300

    initial commit

---
+++ app/src/main/java/com/example/trab_final/MainActivity.kt
@@ -0,0 +1,131 @@
+package com.example.trab_final
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.trab_final.ui.theme.Trab_finalTheme
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.*
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Trab_finalTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    LoginScreen()
+//    Text(text = "Hello $name!")
+}
+
+@Composable
+fun MyImage() {
+    Image(painter = painterResource(id = R.drawable.travel), contentDescription = "Travel")
+}
+
+@Composable
+fun LoginScreen() {
+
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            MyImage()
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            modifier = Modifier.fillMaxWidth(),
+            label = { Text("Username") }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            modifier = Modifier.fillMaxWidth(),
+            label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Button(
+            onClick = { /* Handle login */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Blue
+            )
+        ) {
+            Text("Login", color = Color.White)
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            TextButton(
+                onClick = { /* Handle forgot password */ }
+            ) {
+                Text(
+                    "Esqueci minha senha",
+                    color = Color.Blue
+                )
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            TextButton(
+                onClick = { /* Handle new user */ }
+            ) {
+                Text(
+                    "Novo usuário",
+                    color = Color.Blue
+                )
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Trab_finalTheme {
+        Greeting("Android")
+    }
+}
